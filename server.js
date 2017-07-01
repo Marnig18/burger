14854dcbc7e3a30efce861a50b3d88756b5665c1
Marni Gross <Marni@Marnis-MacBook-Pro.local>
2017-07-01 11:16:35 -0400

    intitial files

---
+++ server.js
@@ -0,0 +1,29 @@
+
+
+///DEPENDECIES
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("methodOverride")
+
+
+//// Creating Server
+var app = express();
+var port = process.env.PORT || 8080;
+
+////Body Parser Code
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+///ROUTER
+require("./controllers/burgers_controller")(app);
+
+
+////Listener
+
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+})
